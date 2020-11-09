40772476b8fe80b5fd9de3e3afcc17f7ad2e2a7d
mkcarter211 <mkcarter@usca.edu>
2020-11-09 13:10:06 -0500

    1 file modified

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyBCTEYvXcpOxTnJF4i9UCfi2NHuDGMf6xE",
+  authDomain: "grocerylist-9d973.firebaseapp.com",
+  databaseURL: "https://grocerylist-9d973.firebaseio.com",
+  projectId: "grocerylist-9d973",
+  storageBucket: "grocerylist-9d973.appspot.com",
+  messagingSenderId: "879915568978",
+  appId: "1:879915568978:web:ba691188eca4a35c95748b",
+  measurementId: "G-CQ4PCHHTX4"
+
 
 };
 
